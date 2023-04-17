b6b5aa78b4127f631393c670e02db3b92fb8b45b
Faiz Rahman <mdfaiz.rahman@gmail.com>
2023-04-17 12:37:20 +0800

    finish trip part

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,13 @@ let isDbReady = false
 
 const SQL_CREATE_TRIP_DETAIL = 'CREATE TABLE IF NOT EXISTS user_trip(trip_id INTEGER PRIMARY KEY AUTOINCREMENT, trip_title TEXT NOT NULL, destination_name TEXT NOT NULL, trip_start_date DATE NOT NULL, risk_assessment_trip NOT NULL, emergency_contact TEXT, contactnum_relationship TEXT, trip_desc TEXT)';
 const SQL_INSERT_NEW_TRIP = 'INSERT INTO user_trip(trip_title, destination_name, trip_start_date, risk_assessment_trip, emergency_contact, contactnum_relationship, trip_desc) VALUES (?, ?, ?, ?, ?, ?, ?)'
-const SQL_SELECT_ALL_TRIP = 'SELECT trip_title,destination_name,trip_start_date FROM user_trip ORDER BY trip_title ASC';
+const SQL_SELECT_ALL_TRIP = 'SELECT trip_id,trip_title,destination_name,trip_start_date FROM user_trip ORDER BY trip_title ASC';
+const SQL_DELETE_ALL_TRIP = 'DELETE FROM user_trip'
+const SQL_RESET_USER_ID = 'DELETE FROM sqlite_sequence WHERE name=`user_trip`'
+const SQL_SELECT_ONE_TRIP = 'SELECT trip_title,destination_name,trip_start_date, risk_assessment_trip, emergency_contact, contactnum_relationship, trip_desc FROM user_trip WHERE trip_id=?';
+const SQL_DELETE_ONE_TRIP = 'DELETE FROM user_trip WHERE trip_id = ?'
+const SQL_UDDATE_ONE_TRIP = 'UPDATE user_trip SET trip_title=?, destination_name=?, trip_start_date=?, risk_assessment_trip=?, emergency_contact=?, contactnum_relationship=?, trip_desc=? WHERE trip_id=?'
+
 
 function onSaveNewTripClicked() {
   if (!isDbReady) {
@@ -37,6 +43,7 @@ function onSaveNewTripClicked() {
           $('#emergency-contact-text').val('')
           $('#contact-relationship-text').val('')
           $('#desc-text').val('')
+
         },
         function (tx, error) { showError('Failed to add trip.') }
       )
@@ -59,7 +66,19 @@ function onShowAllTrip() {
         [],
         function (tx, result) {
           for (let index = 0; index < result.rows.length; index++) {
-
+            // <div class="card card-trip-app mx-3 my-4 position-relative">
+            //   <div class="card-header">
+            //     <span>Trip Name</span>
+            //     <input type="hidden" name="test" value="tripid"></input>
+            //   </div>
+            //   <div class="card-body">
+            //     <blockquote class="blockquote mb-0">
+            //       <p>Destination: Johor, Malaysia</p>
+            //       <footer class="blockquote-footer">Date Start: 12 Apr 23</footer>
+            //     </blockquote>
+            //     <a href="edit_trip.html" class="stretched-link btn-app"></a>
+            //   </div>
+            // </div>
             // Create the card element
             var card = $('<div>', {
               'class': 'card card-trip-app mx-3 my-4 position-relative'
@@ -68,7 +87,12 @@ function onShowAllTrip() {
             // Create the card header element and add it to the card
             var cardHeader = $('<div>', {
               'class': 'card-header'
-            }).append($('<span>').text(`${result.rows.item(index).trip_title}`));
+            }).append($('<span>').text(`${result.rows.item(index).trip_title}`))
+              .append($('<input>', {
+                'type': 'hidden',
+                'id': 'test',
+                'value': `${result.rows.item(index).trip_id}`
+              }));
             card.append(cardHeader);
 
             // Create the card body element and add it to the card
@@ -87,16 +111,17 @@ function onShowAllTrip() {
             cardBody.append(blockquote);
 
             // Create the link element and add its to the card body
-            var link = $('<a>', {
-              'href': 'edit_trip.html',
-              'class': 'stretched-link btn-app'
+            var button = $('<button>', {
+              'class': 'stretched-link hidden-btn-app'
+            }).on('click', function () {
+              sessionStorage.setItem('tripid', `${result.rows.item(index).trip_id}`);
+              window.open("edit_trip.html");
             });
-            cardBody.append(link);
+
+            cardBody.append(button);
 
             // Append the card to the card container element
             $('#cardContainer').append(card);
-
-
 
           }
         },
@@ -108,6 +133,156 @@ function onShowAllTrip() {
   )
 }
 
+function onDeleteTrip() {
+  if (!isDbReady) {
+    showError('Database not ready. Please try again later')
+    return
+  }
+
+  var trip_id_get = sessionStorage.getItem("tripid");
+  db.transaction(
+    function (tx) {
+      tx.executeSql(
+        SQL_DELETE_ONE_TRIP,
+        [trip_id_get],
+        function (tx, result) { //clear ui
+          onShowAllTrip()
+          history.back();
+        },
+        function (tx, error) { showError('Failed to delete trip.') }
+      )
+    },
+    function (error) { },
+    function () { }
+  )
+}
+
+function onUpdateTrip() {
+  if (!isDbReady) {
+    showError('Database not ready. Please try again later')
+    return
+  }
+  let edit_risk_assessment = $('input[name="editriskoptions"]:checked').val().trim();
+  // get input from UI
+  let edit_title = $.trim($('#edit-trip-title-text').val())
+  let edit_destination = $.trim($('#edit-destination-text').val())
+  let edit_start_date = $.trim($('#edit-date-start-text').val())
+  let edit_emergency_contact = $.trim($('#edit-emergency-contact-text').val())
+  let edit_contactnum_relationship = $.trim($('#edit-contact-relationship-text').val())
+  let edit_desc = $.trim($('#edit-desc-text').val())
+  var trip_id_get = sessionStorage.getItem("tripid");
+  if (edit_title === '' || edit_destination === '' || edit_start_date === '' || edit_risk_assessment === '') {
+    showError("Fill in the required field.")
+    return
+  }
+
+  db.transaction(
+    function (tx) {
+      tx.executeSql(
+        SQL_UDDATE_ONE_TRIP,
+        [edit_title, edit_destination, edit_start_date, edit_risk_assessment, edit_emergency_contact, edit_contactnum_relationship, edit_desc, trip_id_get],
+        function (tx, result) { //clear ui
+          onShowAllTrip()
+          history.back();
+        },
+        function (tx, error) { showError('Failed to update trip.') }
+      )
+    },
+    function (error) { },
+    function () { }
+  )
+}
+
+function onShowDetailTrip() {
+  if (!isDbReady) {
+    showError('Database not ready. Please try again later.')
+    return
+  }
+
+  var trip_id_get = sessionStorage.getItem("tripid");
+  db.transaction(
+    function (tx) {
+      tx.executeSql(
+        SQL_SELECT_ONE_TRIP,
+        [trip_id_get],
+        function (tx, result) {
+          if (result.rows.length > 0) {
+            $('#edit-trip-title-text').val(`${result.rows.item(0).trip_title}`)
+            $('#edit-destination-text').val(`${result.rows.item(0).destination_name}`)
+            $('#edit-date-start-text').val(`${result.rows.item(0).trip_start_date}`)
+            $('#edit-emergency-contact-text').val(`${result.rows.item(0).emergency_contact}`)
+            $('#edit-contact-relationship-text').val(`${result.rows.item(0).contactnum_relationship}`)
+            $('#edit-desc-text').val(`${result.rows.item(0).trip_desc}`)
+
+            $('#get_trip_title').text(`${result.rows.item(0).trip_title}`)
+            $('#get_destination').text(`Destination: ${result.rows.item(0).destination_name}`)
+            $('#get_risk_assesment').text(`Risk Assesment: ${result.rows.item(0).risk_assessment_trip}`)
+            $('#get_emergency_contact').text(`Emergency Contact: ${result.rows.item(0).emergency_contact}`)
+            $('#get_contact_relationship').text(` (${result.rows.item(0).contactnum_relationship})`)
+            $('#get_desc').text(`Description: ${result.rows.item(0).trip_desc}`)
+            $('#get_date_start').text(`Date Start: ${result.rows.item(0).trip_start_date}`)
+          }
+
+        },
+        function (tx, error) { showError('Failed to show trip detail.') }
+      )
+    },
+    function (error) { },
+    function () { }
+  )
+}
+
+
+function onResetUserId() {
+  if (!isDbReady) {
+    showError('Database not ready. Please try again later')
+    return
+  }
+
+  db.transaction(
+    function (tx) {
+      tx.executeSql(
+        SQL_RESET_USER_ID,
+        [],
+        function (tx, result) { //clear ui
+
+          onShowAllTrip()
+        },
+        function (tx, error) { showError('Failed to reset id.') }
+      )
+    },
+    function (error) { },
+    function () { }
+  )
+}
+
+function onDeleteAllTrip() {
+  if (!isDbReady) {
+    showError('Database not ready. Please try again later')
+    return
+  }
+
+  db.transaction(
+    function (tx) {
+      tx.executeSql(
+        SQL_DELETE_ALL_TRIP,
+        [],
+        function (tx, result) { //clear ui
+          onResetUserId()
+          onShowAllTrip()
+          history.back();
+        },
+        function (tx, error) { showError('Failed to delete all trip.') }
+      )
+    },
+    function (error) { },
+    function () { }
+  )
+}
+
+
+
+
 function showError(message) {
   navigator.vibrate(2000)
   navigator.notification.beep(1)
@@ -117,6 +292,9 @@ function showError(message) {
 document.addEventListener('deviceready', function () {
   Zepto(function ($) {
     $('#add-user-trip').on('click', onSaveNewTripClicked)
+    $('#edit-user-trip').on('click', onUpdateTrip)
+    $('#delete-user-trip').on('click', onDeleteTrip)
+    $('#delete-all-user-trip').on('click', onDeleteAllTrip)
     db = window.sqlitePlugin.openDatabase(
       { 'name': 'm_expense.db', 'location': 'default' },
       function (database) { // SUCCESS callback
@@ -130,6 +308,11 @@ document.addEventListener('deviceready', function () {
                 isDbReady = true
                 console.log('SQL_CREATE_TRIP_DETAIL', 'OK')
                 onShowAllTrip()
+                onShowDetailTrip()
+                var sessionValue = sessionStorage.getItem("tripid");
+
+                // Set the value of the input element with ID "get_id"
+                $("#get_trip_id").val(sessionValue);
               }, // SUCCESS callback
               function (tx, error) {
                 isDbReady = false
